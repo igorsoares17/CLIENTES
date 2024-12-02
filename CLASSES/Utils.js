8352ec200ccdc98c72ce3d808d069769ec3d0def
igor de souza soares <igorsouzasoares1705@gmail.com>
2024-12-02 13:17:49 -0300

    responsive and working

--- CLASSES/Utils.js
+++ CLASSES/Utils.js
@@ -4,4 +4,17 @@ class Utils {
 
         return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
     }
+
+    static getNumber(data) {
+
+        let array = data.split("v");
+        let number = parseInt(array[0]);
+        return number + 10;
+    }
+
+    static getHeight(number) {
+
+        let height = number.toString() + "vh";
+        return height;
+    }
 }
--- SCRIPTS/Execution.js
+++ SCRIPTS/Execution.js
@@ -5,7 +5,7 @@ class Execution {
         this.form = document.querySelector("form");
         this.inputs = document.querySelectorAll("input");
         this.areaClients = document.getElementById("clients");
-        
+        this.height = "40vh";
         this.submitForm();
         this.getPhoto();
         this.img = "";
@@ -25,21 +25,59 @@ class Execution {
                     
                 }
 
-                if (!this.inputs[3].checked && !this.inputs[4].checked) {
+                else if (!this.inputs[3].checked && !this.inputs[4].checked) {
 
                     window.alert("ESCOLHA O SEXO!");
                     
-                };
+                }
 
-            this.addUser();
+                else {
+
+                    this.addUser();
+                    this.form.reset();
+                }
         });
     }
 
     addUser() {
 
-        this.user = new User(this.inputs[0].value, document.querySelectorAll('input[name="gender"]:checked'), this.inputs[2].value, this.inputs[1].value, this.img, Utils.dateFormat(new Date));
+        this.user = new User(this.inputs[0].value, document.querySelector('input[name="gender"]:checked').value, this.inputs[2].value, this.inputs[1].value, this.img, Utils.dateFormat(new Date));
+
+        let newHeight = Utils.getHeight(Utils.getNumber(this.height));
+        this.height = newHeight;
+        this.areaClients.style.height = this.height;
+
+        let areaNewClient = document.createElement("div");
+        this.areaClients.appendChild(areaNewClient);
+        areaNewClient.setAttribute("class", "area-new-client");
+
+        let areaDatas = []
+
+        for (let i = 0; i < 6; i++) {
+
+            areaDatas[i] = document.createElement("div");
+            areaNewClient.appendChild(areaDatas[i]);
+            areaDatas[i].setAttribute("class", "area-datas"); 
+        }
+
+        areaDatas[0].innerHTML = `<figure class="figure-datas">
+                                       <img class="img-datas" src="${this.img}" alt="Foto cliente">
+                                  </figure>`
+
+        areaDatas[1].innerHTML = `<h3 class="title-datas">NOME</h3>
+                                   <p class="text-datas">${this.user._name}</p>`;
     
-   
+        areaDatas[2].innerHTML = `<h3 class="title-datas">EMAIL</h3>
+                                   <p class="text-datas">${this.user._email}</p>`;
+        areaDatas[3].innerHTML = `<h3 class="title-datas">DATA NASC</h3>
+                                   <p class="text-datas">${this.user._birth}</p>`;
+        
+        areaDatas[4].innerHTML = `<h3 class="title-datas">REGISTRO</h3>
+                                   <p class="text-datas">${this.user._timeRegister}</p>`;
+                        
+        areaDatas[5].innerHTML = `<h3 class="title-datas">SEXO</h3>
+                                   <p class="text-datas">${this.user._gender}</p>`;
+
     }
 
     getPhoto() {
