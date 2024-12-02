6f2791930961297c1abf90c8d32fe885ddbb19d2
igor de souza soares <igorsouzasoares1705@gmail.com>
2024-12-02 17:35:08 -0300

    validating form

--- SCRIPTS/Execution.js
+++ SCRIPTS/Execution.js
@@ -5,7 +5,10 @@ class Execution {
         this.form = document.querySelector("form");
         this.inputs = document.querySelectorAll("input");
         this.areaClients = document.getElementById("clients");
+
+        //ALTURA INICIAL DA DIV CLIENTS PARA O JAVASCRIPT:
         this.height = "40vh";
+
         this.submitForm();
         this.getPhoto();
         this.img = "";
@@ -18,28 +21,58 @@ class Execution {
 
             event.preventDefault();
 
+            //VARIÁVEIS E MÉTODOS PARA GARANTIR QUE OS DADOS DIGITADOS SÃO VÁLIDOS:
 
-                if (!this.inputs[0].value || !this.inputs[1].value || !this.inputs[2].value || !this.inputs[5].value) {
+            let isValidName = this.validInputName(this.inputs[0].value);
 
-                    window.alert("HA CAMPOS NÃO PREENCHIDOS!");
+            //SE O USUÁRIO RETIRAR O REQUIRED DO HTML, O INPUT FICA VERMELHO:
+
+            if (!this.inputs[0].value || !this.inputs[1].value || !this.inputs[2].value) {
+
+                for (let x = 0; x < 3; x++) {
+
+                    if (!this.inputs[x].value) {
+    
+                        this.inputs[x].style.border = "2px solid red";
+                        return;
+                    }
+                }
+            }
+
+            else if (!this.inputs[3].checked && !this.inputs[4].checked) {
+
+                window.alert("Selecione o sexo!");
                     
-                }
+            }
 
-                else if (!this.inputs[3].checked && !this.inputs[4].checked) {
+            else if (this.img == "") {
 
-                    window.alert("ESCOLHA O SEXO!");
+                window.alert("Selecione a foto!");
                     
-                }
+            }
 
-                else {
+            else if (isValidName != true) {
 
-                    this.addUser();
-                    this.form.reset();
+                window.alert(isValidName);
+            }
+
+            else {
+
+                this.addUser();
+                this.form.reset();
+
+                for (let y = 0; y < 3; y++) {
+
+                    this.inputs[y].style.border = "2px solid #000"
                 }
+            }
         });
     }
 
     addUser() {
+
+        let newLetter = this.inputs[0].value.charAt(0).toUpperCase();
+        this.inputs[0].value = this.inputs[0].value.replace(`${this.inputs[0].value.charAt(0)}`, `${newLetter}`);
 
         this.user = new User(this.inputs[0].value, document.querySelector('input[name="gender"]:checked').value, this.inputs[2].value, this.inputs[1].value, this.img, Utils.dateFormat(new Date));
 
@@ -62,7 +95,7 @@ class Execution {
 
         areaDatas[0].innerHTML = `<figure class="figure-datas">
                                        <img class="img-datas" src="${this.img}" alt="Foto cliente">
-                                  </figure>`
+                                  </figure>`;
 
         areaDatas[1].innerHTML = `<h3 class="title-datas">NOME</h3>
                                    <p class="text-datas">${this.user._name}</p>`;
@@ -77,6 +110,8 @@ class Execution {
                         
         areaDatas[5].innerHTML = `<h3 class="title-datas">SEXO</h3>
                                    <p class="text-datas">${this.user._gender}</p>`;
+
+        this.updateCount();
 
     }
 
@@ -98,6 +133,44 @@ class Execution {
                 reader.readAsDataURL(photoInput.files[0])
             }
         })
+    }
+
+    updateCount() {
+
+        let count = 0;
+
+        for (let z = 0; z < this.areaClients.children.length-1; z++) {
+
+            count++;
+        }
+
+        let clientsQuantity = document.querySelector("#title-clients");
+        clientsQuantity.innerHTML = `CLIENTES (${count})`;
+
+    }
+
+    validInputName(name) {
+
+        let vowels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', " "];
+
+        for (let x = 0; x < name.length; x++) {
+
+            if (vowels.indexOf(name[x]) == -1) {
+
+                return "O NOME DEVE CONTER APENAS LETRAS!"
+            }
+
+        }
+
+        if (name.length < 4) {
+
+            return "O NOME DEVE CONTER PELO MENOS QUATRO LETRAS"
+        }
+
+        else {
+
+            return true;
+        }
     }
 }
 
